bbebc55a9ae2484199d2f94ca326eccd530cfdaa
Victor-HM <victor.27cm.santos@gmail.com>
2022-03-25 16:20:08 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Cabecalho from './src/components/cabecalho';
+
+export default function App() {
+  return (
+    <View>
+      <Cabecalho />
+    </View>
+  );
+}
---
+++ src/components/cabecalho/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Cabecalho() {
+  return (
+      <View style={styles.headers}>
+          <Ionicons name='menu' size={24} color='white' />
+          <Text style={styles.Texto}> Play Store </Text>
+
+          <Ionicons name='ios-search-sharp' size={24} color='white' />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    headers: {
+        backgroundColor: '#002333',
+        borderRadius: 2,
+        marginTop: 8,
+        width: '99.9%',
+        padding: 20,
+        margin: 2,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+
+    Texto: {
+        color: 'white',
+        marginLeft: -120,
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
