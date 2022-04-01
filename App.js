521a62e7331617a6503c3f322ef7cd8f48fbe2e4
Victor-HM <victor.27cm.santos@gmail.com>
2022-04-01 16:32:45 -0300

    adiçao de botoes

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Cabecalho from './src/components/cabecalho';
+import Botao from './src/components/botao';
 
 export default function App() {
   return (
     <View>
       <Cabecalho />
+      <Botao logo="game-controller"
+             texto="jogos"
+             cor="black"
+             logo2="game-controller"
+             texto2="jogos 2 !"
+             cor2="black" />
     </View>
   );
 }
---
+++ src/components/botao/index.js
@@ -0,0 +1,45 @@
+import React,{Fragment} from 'react';
+import {View,Image,Text,TouchableOpacity,StyleSheet} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+
+export default function Botao({logo,texto,cor,logo2,texto2,cor2}){
+
+    return(
+        <View style={estilo.containerBotoes1}>
+            <TouchableOpacity style={[estilo.botoes ,{backgroudColor:cor}]}>
+                <Ionicons name={logo} size={20} color="black" style={{marginRight:10}}/>
+                <Text style={estilo.textoBotao} > {texto} </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[estilo.botoes ,{backgroudColor:cor2}]}>
+                <Ionicons name={logo2} size={20} color="black" style={{marginRight:10}}/>
+                <Text style={estilo.textoBotao} > {texto2} </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const estilo = StyleSheet.create({
+
+    containerBotoes1:{
+        flexDirection:'row',
+        marginBottom:10,
+        justifyContent:"center"
+    },
+
+    textoBotao:{
+        color:'#00BFFF',
+        fontWeight:'bold'
+    },
+
+    botoes:{
+        padding:5,
+        marginHorizontal:10,
+        width:'45%',
+        alignItems:'center',
+        justifyContent:'flex-start',
+        
+        fontSize:12,
+        color:'#00BFFF',
+        flexDirection:'row',
+    }
+})
